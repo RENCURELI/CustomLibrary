2f4427b1cde7d611fe4f02c1a2698e31075fa1bc
Pierre Rencurel <pierre.rencurel@ubisoft.com>
2023-03-05 16:04:00 -0500

    Array unit tests

--- TST_DataStructures/test.cpp
+++ TST_DataStructures/test.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <gtest/gtest.h>
 #include "../DataStructures/include/Array.hpp"
 #include "../DataStructures/include/Vector.hpp"
 #include "../DataStructures/include/List.hpp"
@@ -17,6 +18,42 @@ TEST(ArrayTest, ArraySwap)
 
 	EXPECT_EQ(firstArray.back(), 10);
 	EXPECT_EQ(firstArray.front(), 6);
+}
+
+TEST(ArrayTest, ArrayAt)
+{
+	Array<int, 5> testArray{ 1, 2, 3, 4, 5 };
+
+	EXPECT_EQ(testArray.at(0), testArray.front());
+	EXPECT_EQ(testArray.at(testArray.size() - 1), testArray.back());
+
+	// Test for negative index
+	EXPECT_THROW(testArray.at(-1), std::exception);
+
+	// Test for negative overflowing index
+	EXPECT_THROW(testArray.at(testArray.size()), std::exception);
+}
+
+TEST(ArrayTest, ArraySize)
+{
+	Array<int, 0> emptyArray;
+
+	EXPECT_EQ(emptyArray.empty(), true);
+	EXPECT_EQ(emptyArray.size(), 0);
+
+	Array<int, 5> nonEmptyArray;
+
+	EXPECT_EQ(nonEmptyArray.empty(), false);
+	EXPECT_EQ(nonEmptyArray.size(), 5);
+}
+
+TEST(ArrayTest, ArrayFill)
+{
+	Array<int, 3> testArray;
+	testArray.fill(12);
+
+	for (int i = 0; i < testArray.size(); i++)
+		EXPECT_EQ(testArray.at(i), 12);
 }
 
 #pragma endregion ArrayTests
