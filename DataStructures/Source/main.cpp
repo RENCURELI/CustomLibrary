dd350b5a51336c25e65ff76d089d2b2d5ba0e4a1
Pierre Rencurel <pierre.rencurel@ubisoft.com>
2023-04-08 16:08:57 -0400

    Cleaned main to test currently in development structures

--- DataStructures/Source/main.cpp
+++ DataStructures/Source/main.cpp
@@ -6,48 +6,14 @@
 #include "Array.hpp"
 #include "Vector.hpp"
 
+#include "Stack.hpp"
+
 int main()
 {
 	std::cout << "Hello World! \n";
 
 	try
 	{
-		// ARRAY
-		Array<int, 5> test{ 51, -12, 0, 48, 3 };
-		int val = test.at(3);
-		Array<int, 5> test2{ 3, 21, 98, 4, -1 };
-
-		test.swap(test2);
-
-		// LISTS
-		List<int> intList = { 1, 10, 5, -5 };
-		List<int> newList = { 31, 47 };
-
-		const auto initializerList = { 51, -12, 0 };
-		intList.InsertAt(14, 4);
-		intList.pop_back();
-		intList.RemoveAt(1);
-
-		intList.PrintList();
-
-		newList = intList;
-
-		std::cout << "\n" << intList.GetSize() << "\n";
-		std::cout << intList.IsEmpty() << "\n";
-
-		newList.PrintList();
-
-		std::cout << "\n" << newList.GetSize() << "\n";
-		std::cout << newList.IsEmpty() << "\n";
-
-		newList = initializerList;
-
-		newList.PrintList();
-
-		std::cout << "\n" << newList.GetSize() << "\n";
-		std::cout << newList.IsEmpty() << "\n";
-
-
 		// VECTOR
 		Vector<float> vec{ 5.0f, 1.5f, 0.25f, 10.f };
 		std::cout << "\n" << vec.getCapacity() << "\n";
