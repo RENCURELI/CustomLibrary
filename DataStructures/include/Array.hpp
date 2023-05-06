c196ace57e4a7a68a0dec9a69f9574852cbc2640
Pierre Rencurel <pierre.rencurel@ubisoft.com>
2023-05-06 14:55:13 -0400

    Prep work for Array iterators

--- DataStructures/include/Array.hpp
+++ DataStructures/include/Array.hpp
@@ -2,11 +2,197 @@
 #include <iostream>
 #include <cstddef>
 #include <stdexcept>
+#include <iterator>
+
+// #pragma region ConstIterator
+// template<typename Array>
+// class ArrayConstIterator
+// {
+// public:
+// 	using iterator_concept = std::contiguous_iterator_tag;
+// 	using value_type = typename Array::value_type;
+// 	using pointer = Array::const_pointer;
+// 	using reference = const value_type&;
+// 	using difference_type = std::ptrdiff_t;
+// 
+// 	using PtrType = typename Array::pointer;
+// 
+// 	ArrayConstIterator(PtrType ptr) : m_Ptr(ptr) {}
+// 
+// 	// preincrement
+// 	ArrayConstIterator& operator++()
+// 	{
+// 		this->m_Ptr++;
+// 		return *this;
+// 	}
+// 
+// 	// postincrement
+// 	ArrayConstIterator operator++(int)
+// 	{
+// 		ArrayConstIterator it(*this);
+// 		++(*this);
+// 		return it;
+// 	}
+// 
+// 	// preincrement
+// 	ArrayConstIterator& operator--()
+// 	{
+// 		this->m_Ptr--;
+// 		return *this;
+// 	}
+// 
+// 	// postincrement
+// 	ArrayConstIterator operator--(int)
+// 	{
+// 		ArrayConstIterator it(*this);
+// 		--(*this);
+// 		return it;
+// 	}
+// 
+// 	ArrayConstIterator& operator+=(const difference_type offset)
+// 	{
+// 		this->m_Ptr += offset;
+// 		return *this;
+// 	}
+// 
+// 	ArrayConstIterator operator+(const difference_type offset)
+// 	{
+// 		ArrayConstIterator tmp = this->m_Ptr;
+// 		tmp += offset;
+// 		return tmp;
+// 	}
+// 
+// 	ArrayConstIterator& operator-=(const difference_type offset)
+// 	{
+// 		this->m_Ptr -= offset;
+// 		return *this;
+// 	}
+// 
+// 	ArrayConstIterator operator-(const difference_type offset)
+// 	{
+// 		ArrayConstIterator tmp = this->m_Ptr;
+// 		tmp -= offset;
+// 		return tmp;
+// 	}
+// 
+// 	difference_type operator-(const ArrayConstIterator& other)
+// 	{
+// 		return m_Ptr - other.m_Ptr;
+// 	}
+// 
+// 	const reference operator*() const { return *m_Ptr; }
+// 	reference operator[](int index) { return *(m_Ptr + index); }
+// 	const pointer operator->() const { return m_Ptr; }
+// 	bool operator==(const ArrayConstIterator& other) const { return m_Ptr == other.m_Ptr; }
+// 	bool operator!=(const ArrayConstIterator& other) const { return !(*this == other); }
+// 	bool operator<(const ArrayConstIterator& right) const { return m_Ptr < right.m_Ptr; }
+// 	bool operator>(const ArrayConstIterator& right) const { return right < *this; }
+// 	bool operator<=(const ArrayConstIterator& right) const { return !(right < *this); }
+// 	bool operator>=(const ArrayConstIterator& right) const { return !(*this < right); }
+// 
+// 	PtrType m_Ptr;
+// };
+// 
+// #pragma endregion ConstIterator
+// 
+// #pragma region Iterator
+// template<typename Array>
+// class ArrayIterator : public ArrayConstIterator<Array>
+// {
+// public:
+// 	using value_type = typename Array::value_type;
+// 	using pointer = Array::pointer;
+// 	using reference = value_type&;
+// 	using difference_type = std::ptrdiff_t;
+// 	using BaseIt = ArrayConstIterator<Array>;
+// public:
+// 	// preincrement
+// 	ArrayIterator& operator++()
+// 	{
+// 		BaseIt::operator++();
+// 		return *this;
+// 	}
+// 
+// 	// postincrement
+// 	ArrayIterator operator++(int)
+// 	{
+// 		ArrayIterator it(*this);
+// 		++(*this);
+// 		return it;
+// 	}
+// 
+// 	// preincrement
+// 	ArrayIterator& operator--()
+// 	{
+// 		BaseIt::operator--();
+// 		return *this;
+// 	}
+// 
+// 	// postincrement
+// 	ArrayIterator operator--(int)
+// 	{
+// 		ArrayIterator it(*this);
+// 		--(*this);
+// 		return it;
+// 	}
+// 
+// 	ArrayIterator& operator+=(const difference_type offset)
+// 	{
+// 		BaseIt::operator+=(offset);
+// 		return *this;
+// 	}
+// 
+// 	ArrayIterator operator+(const difference_type offset) const
+// 	{
+// 		ArrayIterator tmp = *this;
+// 		tmp += offset;
+// 		return tmp;
+// 	}
+// 
+// 	ArrayIterator& operator-=(const difference_type offset)
+// 	{
+// 		BaseIt::operator-=(offset);
+// 		return *this;
+// 	}
+// 
+// 	ArrayIterator operator-(const difference_type offset) const
+// 	{
+// 		ArrayIterator tmp = *this;
+// 		tmp -= offset;
+// 		return tmp;
+// 	}
+// 
+// 	difference_type operator-(const ArrayIterator& other)
+// 	{
+// 		return BaseIt::operator-(other);
+// 	}
+// 
+// 	reference operator*() const { return const_cast<reference>(BaseIt::operator*()); }
+// 	reference operator[](int index) { return *(this->m_Ptr + index); }
+// 	pointer operator->() const { return this->m_Ptr; }
+// 	bool operator==(const ArrayIterator& other) const { return this->m_Ptr == other.m_Ptr; }
+// 	bool operator!=(const ArrayIterator& other) const { return !(*this == other); }
+// 	bool operator<(const ArrayIterator& right) const { return this->m_Ptr < right.m_Ptr; }
+// 	bool operator>(const ArrayIterator& right) const { return right < *this; }
+// 	bool operator<=(const ArrayIterator& right) const { return !(right < *this); }
+// 	bool operator>=(const ArrayIterator& right) const { return !(*this < right); }
+// };
+// #pragma endregion Iterator
 
 #pragma warning(disable : 4200) // We disable this warning as we assign the value of N on construction -> https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-levels-2-and-4-c4200?f1url=%3FappId%3DDev16IDEF1%26l%3DEN-US%26k%3Dk(C4200)%26rd%3Dtrue&view=msvc-170
 template<typename T, const unsigned int N>
 struct Array
 {
+// 	using value_type = T;
+// 	using pointer = T*;
+// 	using const_pointer = const T*;
+// 	using iterator = ArrayIterator<Array<T, N>>;
+// 	using const_iterator = ArrayConstIterator<Array<T, N>>;
+// 	using reverse_iterator = std::reverse_iterator<iterator>;
+// 	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+	// size_type would be the unsigned version of the allocator_traits::difference_type ( I currently don't have an allocator or allocator_traits )
+	//using size_type = std::make_unsigned<const_iterator::difference_type>;
+
 	T values[N];
 
 	T& operator[](unsigned int index)
