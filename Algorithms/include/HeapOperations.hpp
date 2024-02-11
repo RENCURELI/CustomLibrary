0539eadc6a5729836f4d028e50524c1495fb23be
Pierre Rencurel <pierre.rencurel@ubisoft.com>
2024-02-11 17:37:01 -0500

    Continued investigating MakeHeap algorithm

--- Algorithms/include/HeapOperations.hpp
+++ Algorithms/include/HeapOperations.hpp
@@ -1,5 +1,9 @@
 // For all Heap Operations -> https://en.cppreference.com/w/cpp/algorithm#Heap_operations
 
+// References
+//https://github.com/llvm/llvm-project/blob/main/libcxx/include/__algorithm/make_heap.h
+//https://github.com/llvm/llvm-project/blob/main/libcxx/include/__algorithm/sift_down.h
+
 #pragma once
 
 #include <algorithm>
@@ -9,10 +13,12 @@ template<std::random_access_iterator RandomIt, class Compare>
 constexpr void MakeHeap(RandomIt first, RandomIt last, Compare comp)
 {
 	std::ptrdiff_t bottom = last - first;
-	for (std::ptrdiff_t hole = bottom >> 1; hole > 0;)
+	// Start from "lowest" parent and go "up" towards first parent
+	for (std::ptrdiff_t hole = bottom >> 1; hole > 0;) // Divide by two ( shift for better assembly )
 	{
 		--hole;
 		std::_Iter_value_t<RandomIt> val = std::move(*(first + hole));
+		//SiftDown();
 	}
 }
 
