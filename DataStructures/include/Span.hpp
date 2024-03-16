f4d6a0c67e50de1160ea8dc3ccdbf26815fd74ca
Pierre Rencurel <pierre.rencurel@ubisoft.com>
2024-03-16 15:16:22 -0400

    Span Constructors WIP
    Continued working on Span constructors
    Adding structure for future test cases

--- DataStructures/include/Span.hpp
+++ DataStructures/include/Span.hpp
@@ -97,6 +97,12 @@ struct SpanIterator
 
 #pragma endregion Iterator
 
+template<class OtherT, class T>
+concept _Span_array_convertible = requires(T elementType, OtherT otherType)
+{
+	std::is_convertible_v<OtherT(*)[], T(*)[]>;
+};
+
 template<class T, std::size_t Extent>
 class Span
 {
@@ -116,44 +122,52 @@ public:
 	static constexpr size_type extent = Extent;
 
 	constexpr Span() noexcept requires (Extent == 0) : m_Data{ nullptr } {}
-	constexpr explicit Span(std::initializer_list<value_type> initList) requires std::is_const_v<element_type> : m_Data{ initList.begin() }
-	{
-		static_assert(Extent == initList.size(), "[ERROR] Size mismatch in span's constructor Extent != initList.size()");
-	}
+	// Might require CXX 26
+// 	constexpr explicit Span(std::initializer_list<value_type> initList) requires std::is_const_v<element_type> : m_Data{initList.begin()}
+// 	{
+// 		static_assert(Extent == initList.size(), "[ERROR] Size mismatch in span's constructor Extent != initList.size()");
+// 	}
 	
-	template<std::_Span_compatible_iterator It>
-	constexpr Span(It first, size_type count) : m_Data{ std::to_address(first) }
+	template<std::_Span_compatible_iterator<element_type> It>
+	constexpr explicit Span(It first, size_type count) : m_Data{ std::to_address(first) }
 	{
 		(void)count;
-		static_assert(Extent == count, "[ERROR] Size mismatch in span constructor (iterator, len)");
+		//static_assert(Extent == count, "[ERROR] Size mismatch in span constructor (iterator, len)");
+		assert(Extent == count);
 	}
 
 	template <size_t _Size>
 	constexpr Span(std::type_identity_t<element_type>(&arr)[_Size]) noexcept : m_Data{arr} {}
 
+	template<_Span_array_convertible<element_type> U, std::size_t N> // U's type will be inferred and set as first argument of concept
+	constexpr Span(std::array<U, N>& arr) noexcept : m_Data{ arr.data() } {}
+
+	template<_Span_array_convertible<element_type> U, std::size_t N> // U's type will be inferred and set as first argument of concept
+	constexpr Span(const std::array<U, N>& arr) noexcept : m_Data{ arr.data() } {}
+
 private:
 	pointer m_Data;
 };
 
-//	template<class T>
-//	class Span<T, std::dynamic_extent>
-//	{
-//	public:
-//		using element_type = T;
-//		using value_type = std::remove_cv_t<T>;
-//		using size_type = std::size_t;
-//		using difference_type = std::ptrdiff_t;
-//		using pointer = T*;
-//		using const_pointer = const T*;
-//		using reference = T&;
-//		using const_reference = const T&;
-//		using iterator = SpanIterator<T>;
-//		using reverse_iterator = std::reverse_iterator<iterator>;
-//		//using const_iterator -> requires c++ 23 ( std::const_iterator<iterator> )
-//	
-//		static constexpr size_type extent = std::dynamic_extent;
-//	
-//	private:
-//		pointer m_Data;
-//		size_type m_Size;
-//	};
+	template<class T>
+	class Span<T, std::dynamic_extent>
+	{
+	public:
+		using element_type = T;
+		using value_type = std::remove_cv_t<T>;
+		using size_type = std::size_t;
+		using difference_type = std::ptrdiff_t;
+		using pointer = T*;
+		using const_pointer = const T*;
+		using reference = T&;
+		using const_reference = const T&;
+		using iterator = SpanIterator<T>;
+		using reverse_iterator = std::reverse_iterator<iterator>;
+		//using const_iterator -> requires c++ 23 ( std::const_iterator<iterator> )
+	
+		static constexpr size_type extent = std::dynamic_extent;
+	
+	private:
+		pointer m_Data;
+		size_type m_Size;
+	};
--- TST_DataStructures/ContainerTests/SpanTest.hpp
+++ TST_DataStructures/ContainerTests/SpanTest.hpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <span>
+#include <vector>
 
 #pragma region SpanConstructorTests
 // ================================================
@@ -14,7 +15,13 @@ TEST(SpanTest, Constructor)
 {
 	int myArray[]{1, 2, 3, 4, 5};
 	Span<int, 5> testSpan(myArray);
-	//std::span<int> testSpan(myArray);
+
+	std::vector<int> vec = { 1, 2, 3, 4, 5 };
+	Span<int, 5> vecSpan(vec.begin(), 5);
+	//Span<int, 5> vecSpanFailAssert(vec.begin(), 4); -> Will need to either test for assert, or make static_assert work
+
+	std::array<int, 5> tstArray{ 1, 2, 3, 4, 5 };
+	Span<int, 5> arraySpan(tstArray);
 }
 
 #pragma endregion SpanConstructorTests
